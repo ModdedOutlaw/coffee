a881c6b883613abb8edae127e955211a5c9094db
ModdedOutlaw <moddedoutlaw33@gmail.com>
2022-07-07 06:25:51 -0400

    Initial commit

---
+++ js/cart.js
@@ -0,0 +1,211 @@
+import { getLocalStorage, setLocalStorage } from './script.js';
+
+import { displayCartIcon } from './products.js';
+
+const currentCartItemsContainer = document.querySelector('.item');
+let totalPrice = document.querySelector('.totalPrice');
+let currentPrice = [];
+
+updateWaxPrice();
+
+
+
+function selectElementsForCart(card) {
+  const image = card.querySelector('img').src;
+  const id = card.id;
+  const title = card.querySelector('h3').innerText;
+
+  return [image, id, title];
+}
+
+function displayCartItems(currentCart) {
+  
+  updateWaxPrice();
+
+  currentCartItemsContainer.innerHTML = '';
+
+  currentPrice = [];
+
+  currentCart.forEach((item) => {
+
+    const currentQtyPrice = Number(item.price) * item.qty;
+    currentPrice.push(currentQtyPrice);
+   
+    currentCartItemsContainer.innerHTML += `
+    <div class="cartItem" id=${item.id}>
+        <div class="cartInfo">
+          <h3 class="product__1--title">${item.title}</h3>
+          <div class="product__1--image">
+            <img
+              class="image--1"
+              src="${item.image}"
+              alt="product 1"
+              class="product-image"
+            />
+        </div>
+        <h5 class="product__1--price">${item.price} $</h5>
+      </div>
+      <div class="buttons-action">
+        <form class="form">
+          <label for="quantity">Quantity:</label>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            class="quantity"
+            min="1"
+            max="50"
+            value="${item.qty}"
+            autofocus
+          />
+        </form>
+        <button class="btn add-cart deleteBtn">Delete</button>
+      </div>
+    </div>
+    `;
+  });
+
+
+
+  totalPrice.innerText = currentPrice
+    .reduce((curr, acc) => curr + acc, 0)
+    .toFixed(2);
+
+     console.log(currentPrice.reduce((curr, acc) => curr + acc, 0).toFixed(2));
+
+
+    console.log(sessionStorage.getItem('wax_price'));
+
+    let total_wax_session = currentPrice.reduce((curr, acc) => curr + acc, 0).toFixed(2) / sessionStorage.getItem('wax_price');
+
+    console.log("TOTAL PRICE SESSION $$$$ = " + totalPrice.innerText);
+
+    document.getElementById('total_wax_order').textContent = '$'+totalPrice.innerText+ ' = '+total_wax_session.toFixed(8).toString() +' wax';
+
+    console.log("TOTAL PRICE SESSION WAX = " + total_wax_session);
+
+    console.log('QUANTITY IN CART = ' + getLocalStorage('qtyCart'));
+
+    document.getElementById('cart_total').textContent = getLocalStorage('qtyCart');
+
+
+
+
+
+    sessionStorage.setItem('final_price_wax',total_wax_session.toFixed(8).toString());
+
+    sessionStorage.setItem('final_price_usd',totalPrice.innerText);
+
+    console.log(currentCart);
+
+    currentCart.forEach((item) =>{
+       sessionStorage.setItem(item.id,item.qty.toString());
+    } );
+
+  
+    
+    
+    console.log("TOTAL 12ozEspresso = " + sessionStorage
+    .getItem('1'));
+console.log("TOTAL 12ozJitteryBean = " +
+    sessionStorage.getItem('2'));
+console.log("TOTAL 12ozJitteryGround = " +
+    sessionStorage.getItem('3'));
+
+console.log("TOTAL 12ozOGBean = " + sessionStorage
+    .getItem('4'));
+console.log("TOTAL 12ozOGEspresso = " +
+    sessionStorage.getItem('5'));
+console.log("TOTAL 12ozOGGround = " + sessionStorage
+    .getItem('6'));
+
+  loadCartListeners();
+}
+
+
+async function fetchUpdateWaxPrice() {
+
+  const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=wax&vs_currencies=usd');
+
+  const wax_price = await response.json();
+
+  return wax_price;
+
+}
+
+
+async function updateWaxPrice() {
+
+
+  await fetchUpdateWaxPrice().then(price => {
+
+
+
+      //localStorage.setItem('price_wax',price.wax.usd);
+      sessionStorage.setItem('wax_price',price.wax.usd);
+
+  });
+
+}
+
+
+
+
+
+
+function loadCartListeners() {
+  const deleteBtn = document.querySelectorAll('.deleteBtn');
+  const qtyInput = document.querySelectorAll('.quantity');
+  const form = document.querySelectorAll('.form');
+
+  deleteBtn.forEach((btn) => btn.addEventListener('click', handleDelete));
+  form.forEach((el) =>
+    el.addEventListener('submit', (e) => e.preventDefault())
+  );
+  qtyInput.forEach((btn) => btn.addEventListener('change', handleChange));
+}
+
+function handleDelete(e) {
+  const id = e.target.closest('.cartItem').id;
+  const qty = e.target.previousElementSibling.querySelector('.quantity').value;
+
+  const currentCart = getLocalStorage('currentCart');
+  const currentQty = getLocalStorage('qtyCart');
+
+  const filteredCart = currentCart.filter((product) => product.id !== id);
+  const filteredQty = currentQty - qty;
+
+  setLocalStorage('currentCart', filteredCart);
+  setLocalStorage('qtyCart', filteredQty);
+
+  sessionStorage.setItem(id, '0');
+
+
+}
+
+function handleChange(e) {
+  const newQty = e.target.closest('.quantity').value;
+  const id = e.target.closest('.cartItem').id;
+
+  const currentCart = getLocalStorage('currentCart');
+
+  const filteredCart = currentCart.map((product) =>
+    product.id === id ? { ...product, qty: newQty } : { ...product }
+  );
+
+  const filteredQty = filteredCart.reduce(
+    (acc, curr) => acc + Number(curr.qty),
+    0
+  );
+
+
+
+  setLocalStorage('currentCart', filteredCart);
+  setLocalStorage('qtyCart', filteredQty);
+}
+
+
+
+
+
+export { displayCartItems, selectElementsForCart };
---
+++ loginWCW.js
@@ -0,0 +1,405 @@
+
+
+let waxUser;
+
+let txID = '';
+
+let waxObj;
+
+let session;
+
+//let current_price_of_wax = 0;
+
+//let current_price_of_coffee = 0;
+
+let total_sent;
+
+//sessionStorage.setItem('usd_price','18.99');
+
+
+//automatically check for credentials
+//autoLogin();
+
+
+checkPriceofwax();
+
+const wax = new waxjs.WaxJS({
+    rpcEndpoint: 'https://wax.greymass.com'
+});
+
+
+
+//checks if autologin is available 
+async function autoLogin() {
+    let isAutoLoginAvailable = await wax.isAutoLoginAvailable();
+    if (isAutoLoginAvailable) {
+        let userAccount = wax.userAccount;
+        let pubKeys = wax.pubKeys;
+
+    } else {
+        //document.getElementById('autologin').insertAdjacentHTML('beforeend', 'Not auto-logged in');
+    }
+}
+
+//normal login. Triggers a popup for non-whitelisted dapps
+async function loginWCW() {
+    try {
+
+
+        //if autologged in, this simply returns the userAccount w/no popup
+        let userAccount = await wax.login();
+
+        checkBalance(userAccount);
+        let pubKeys = wax.pubKeys;
+        //let str = 'Account: ' + userAccount + '<br/>Active: ' + pubKeys[0] + '<br/>Owner: ' + pubKeys[1]
+
+
+        await wax.login(identifier).then((result) => {
+            console.log("--------" + result);
+
+            document.body.classList.add('logged-in');
+            document.body.classList.remove('logged-in-wcw');
+
+            // document.getElementById('wax-addr').textContent = result;
+            document.getElementById('account-name').textContent = userAccount;
+
+            session = wax;
+
+            let waxString = JSON.stringify(wax);
+
+            waxObj = JSON.parse(waxString);
+
+            console.log('WAXOBJ: ' + wax.userAccount);
+            waxUser = wax.userAccount;
+            sessionStorage.setItem('name', waxUser);
+            
+            //didLogin();
+        })
+    } catch (e) {
+        console.log("++++++" + e);
+    }
+
+
+}
+
+function logoutWCW() {
+    console.log("LOGOUT WCW");
+
+    document.body.classList.remove('logged-in-wcw');
+
+
+    //session.remove();
+    //location.reload()
+    sessionStorage.clear();
+    localStorage.clear();
+    document.location.reload();
+}
+
+async function transferWaxWCW() {
+
+    try {
+
+    total_sent = "0.01000000";
+      // total_sent = sessionStorage.getItem('final_price_wax');
+       let text2 = "WAX";
+       let send_amount = total_sent.concat(" ", text2);
+       
+
+
+        if (anchorLogin) {
+            console.log('Key Type = '+keyType);
+            const resultTrans = await link.transact({
+                actions: [{
+                    account: 'eosio.token',
+                    name: 'transfer',
+                    authorization: [{
+                        actor: waxUser,
+                        permission: keyType,
+                    }],
+                    data: {
+                        from: waxUser,
+                        to: 'd.2b4.wam',
+                        quantity: send_amount,
+                        memo: 'Thanks for the coffee!'
+                    },
+                }]
+            }, {
+                blocksBehind: 3,
+                expireSeconds: 30
+            });
+
+            txID = resultTrans.payload.tx;
+
+            const resultsJson = JSON.stringify(resultTrans, getCircularReplacer());
+
+            console.log('RESULTS -----> ' + resultsJson);
+
+            txID = resultTrans.payload.tx;
+
+            if (resultTrans.processed.receipt.status == 'executed') {
+                document.getElementById("pay").style.display = "none";
+                document.getElementById("ship").style.display = "block";
+                document.getElementById("shipping-form").style.display = "block";
+            }
+
+
+        } else {
+            const resultTrans = await wax.api.transact({
+                actions: [{
+                    account: 'eosio.token',
+                    name: 'transfer',
+                    authorization: [{
+                        actor: waxUser,
+                        permission: 'active',
+                    }],
+                    data: {
+                        from: waxUser,
+                        to: 'd.2b4.wam',
+                        quantity: send_amount,
+                        memo: 'Thanks for the coffee!'
+                    },
+                }]
+            }, {
+                blocksBehind: 3,
+                expireSeconds: 30
+            });
+
+            txID = resultTrans.transaction_id;
+
+            if (resultTrans.processed.receipt.status == 'executed') {
+                document.getElementById("pay").style.display = "none";
+                document.getElementById("ship").style.display = "block";
+                document.getElementById("shipping-form").style.display = "block";
+            }
+        }
+
+
+    } catch (e) {
+        console.log(e);
+        document.getElementById('response').append(e.message);
+    }
+
+
+    return txID;
+
+}
+
+function hideBtn() {
+    document.getElementById("pay").style.display = "none";
+    document.getElementById("ship").style.display = "none";
+}
+
+async function checkTransaction(id) {
+
+
+    await fetchTransaction(id).then(transaction_info => {
+        return transaction_info.executed;
+    });
+
+}
+
+
+
+async function checkBalance(wam) {
+
+
+    await fetchBalance(wam).then(balance_info => {
+        console.log("INFO--->" + JSON.stringify(balance_info.balances));
+
+        const balanceJson = JSON.stringify(balance_info.balances);
+
+        balanceObj = JSON.parse(balanceJson);
+        const result = balanceObj.find(({
+            currency
+        }) => currency === 'WAX');
+        console.log(result.amount);
+
+        let balance = Number(result.amount).toFixed(2).toString();
+
+        document.getElementById('user-balance').textContent = balance;
+        document.getElementById("wax-balance").style.display = "block";
+        
+
+        return balance;
+    });
+
+}
+
+
+async function fetchBalance(wam) {
+
+    const response = await fetch('https://lightapi.eosamsterdam.net/api/balances/wax/' + wam);
+
+    const balance = await response.json();
+
+    return balance;
+
+}
+
+
+async function fetchTransaction(transaction_id) {
+
+    const response = await fetch('https://api.waxsweden.org:443/v2/history/get_transaction?id=' + transaction_id);
+
+    const transaction = await response.json();
+
+    return transaction;
+
+}
+
+async function fetchWaxPrice() {
+
+    const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=wax&vs_currencies=usd');
+
+    const wax_price = await response.json();
+
+    return wax_price;
+
+}
+
+async function checkPriceofwax() {
+
+
+    await fetchWaxPrice().then(price => {
+
+        let current_price_of_wax = price.wax.usd;
+
+        //localStorage.setItem('price_wax',price.wax.usd);
+        sessionStorage.setItem('wax_price',price.wax.usd);
+
+        console.log('PRICE OF WAX --- ' + sessionStorage.getItem('wax_price'));
+
+        
+
+
+
+        let usd_price = 18.99;
+
+        let current_price_of_coffee = (usd_price / current_price_of_wax).toFixed(8);
+
+        console.log(current_price_of_coffee);
+
+        document.getElementById('wax-price').textContent = current_price_of_wax;
+        document.getElementById('dollar-cost').textContent = usd_price;
+        document.getElementById('wax-cost').textContent = current_price_of_coffee;
+
+        return current_price_of_coffee;
+    });
+
+}
+
+
+
+
+
+//ANCHOR LOGIN SECTION
+
+// app identifier, should be set to the eosio contract account if applicable
+const identifier = 'moddedoutlaw';
+// initialize the browser transport
+const transport = new AnchorLinkBrowserTransport();
+// initialize the link
+const link = new AnchorLink({
+    transport,
+    chains: [{
+        chainId: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
+        nodeUrl: 'https://wax.greymass.com',
+    }]
+});
+// the session instance, either restored using link.restoreSession() or created with link.login()
+let sessionA;
+
+let loginObj;
+
+let linkObj;
+
+let anchorLogin = false;
+
+let keyType = '';
+
+// tries to restore session, called when document is loaded
+function restoreSession() {
+    link.restoreSession(identifier).then((result) => {
+        sessionA = result;
+        if (sessionA) {
+            didLogin();
+        }
+    })
+}
+
+// login and store session if sucessful
+function login() {
+
+    link.login(identifier).then((result) => {
+        console.log(result);
+        keyType = result.payload.sp;
+        sessionA = result.session;
+        didLogin();
+    })
+}
+
+// logout and remove session from storage
+function logout() {
+    document.body.classList.remove('logged-in');
+    sessionStorage.clear();
+    document.location.reload();
+    sessionA.remove();
+
+
+}
+
+// called when session was restored or created
+function didLogin() {
+
+    
+    document.getElementById('account-name').textContent = sessionA.auth.actor;
+    sessionStorage.setItem('name', waxUser);
+
+    checkBalance(sessionA.auth.actor);
+ 
+
+    document.body.classList.add('logged-in');
+    document.body.classList.remove('app-ui');
+    document.body.classList.remove('logged-in-wcw');
+
+    const loginJson = JSON.stringify(sessionA, getCircularReplacer());
+    loginObj = JSON.parse(loginJson);
+
+    console.log("SESSION INFO: "+loginObj);
+
+    const linkJson = JSON.stringify(link, getCircularReplacer());
+
+    linkObj = JSON.parse(linkJson);
+
+    waxUser = sessionA.auth.actor;
+
+    sessionStorage.setItem('name', waxUser);
+
+    anchorLogin = true;
+
+
+
+}
+
+const getCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key, value) => {
+        if (typeof value === 'object' && value !== null) {
+            if (seen.has(value)) {
+                return;
+            }
+            seen.add(value);
+        }
+        return value;
+    };
+};
+
+async function buyCoffee() {
+
+    let transfer_id = await transferWaxWCW();
+
+    console.log('transaction id = ' + transfer_id);
+
+    sessionStorage.setItem('transferId', transfer_id)
+
+}
